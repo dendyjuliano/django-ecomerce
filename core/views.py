275e3eef2e7f0ee7e51ebbc09902952d87ff978e
Dendy <dendyjuliano2019@gmail.com>
2021-02-09 13:38:07 +0700

    initial project

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Item
 
-# Create your views here.
+def item_list(request):
+    context = {
+        'items':Item.objects.all()
+    }
+    return render(request,"home-page.html",context)
+
+def checkout(request):
+    return render(request,"checkout-page.html")
+
+def product(request):
+    return render(request,"product-page.html")
